8f66e9828d19cd8452b00ce3fb59087d72bf20dd
aliniluca <115418094+aliniluca@users.noreply.github.com>
2024-09-30 23:10:45 +0300

    Update index.ts

--- pages/api/listings/index.ts
+++ pages/api/listings/index.ts
@@ -1,31 +1,22 @@
 import prisma from "@/utils/prisma";
-import { Listing } from '@prisma/client'; // If you are using Prisma
 import { NextApiRequest, NextApiResponse } from "next";
 import { Session, getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
-
-async function CreateNewAd(adData: Listing) {
-  try {
-    const newAd = await prisma.listing.create({
-      data: adData,
-    });
-    return newAd;
-  } catch (error: any) {
-    throw new Error(error);
-  }
-}
-
-// POST '/api/listings/'
-export default async function Handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  const session: Session | null = await getServerSession(req, res, authOptions);
-
   if (session) {
     // POST '/api/listings/'
     if (req.method === "POST") {
+      console.log("sdfs");
+      // @ts-expect-error - By default, session.user doesn't have ID. I added it using callbacks in `pages/api/auth/[...nextauth.ts]`
+      const userId = session.user?.id;
+      console.log(userId);
       try {
+        // const adData = {
+        //     userId,
+        //     ...req.body,
+        //     price: parseInt(req.body.price, 10),
+        // };
+        // console.log("Data from server", adData);
+        // const newAd = await CreateNewAd(adData);
         const userId = session.user?.id;
         console.log(userId);
         
@@ -33,7 +24,7 @@ export default async function Handler(
 
         res.status(201);
       } catch (error: any) {
-        console.error("API error:", error);
+        console.error("API error:", error); // Add this line to log the error
         res.status(500).send({ error: error.message });
       }
     } else {
