03491c641832cc620cbc4f2d97459708d3e08cf2
aliniluca <115418094+aliniluca@users.noreply.github.com>
2024-10-13 20:30:17 +0300

    Update [id].ts

--- pages/api/listings/[id].ts
+++ pages/api/listings/[id].ts
@@ -1,34 +1,31 @@
-import prisma from "@utils/prisma";
+import prisma from "@/utils/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function getListing(id: string) {
-  const listing = await prisma.listing.findUnique({
-    where: { id },
+  return prisma.listing.findUnique({
+    where: {
+      id: id,
+    },
     include: {
       user: true,
-      images: true, // assuming images is an array of image objects
+      images: true,
       category: true,
       favorites: true,
       tags: true,
       messages: true,
     },
   });
-
-  // Check if listing has images and sanitize the images array
-  if (listing?.images) {
-    listing.images = listing.images.map(image => ({
-      ...image,
-      url: image.url || null, // Fallback if url is missing
-    }));
-  }
-
-  return listing;
 }
 
-export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
+// GET '/api/listings/[id]'
+export default async function Handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "GET") {
     const id: string = req.query.id as string;
 
+    // Input validation
     if (!id) {
       return res.status(400).send("Missing id parameter in the request.");
     }
@@ -36,13 +33,11 @@ export default async function Handler(req: NextApiRequest, res: NextApiResponse)
     try {
       const listingInfo = await getListing(id);
 
-      if (!listingInfo) {
-        return res.status(404).json({ error: "Listing not found." });
-      }
-
       return res.status(200).json(listingInfo);
-    } catch (error) {
-      return res.status(500).json({ message: "An error occurred.", error: error.message });
+    } catch (error: any) {
+      res
+        .status(500)
+        .json({ message: "An error occurred.", error: error.message });
     }
   } else {
     res.setHeader("Allow", "GET");
